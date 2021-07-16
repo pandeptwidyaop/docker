b775325b9bc20efe251fa58a4564b831582c783b
Pande <widya.oktapratama@gmail.com>
2021-07-16 16:46:32 +0800

    Add git to docker image

--- laravel-php8-alpine3.14/build.sh
+++ laravel-php8-alpine3.14/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker build \
-    -t pandeptwidyaop/laravel:php8-alpine3.14-v0.1 \
+    -t pandeptwidyaop/laravel:php8-alpine3.14-v0.2 \
     -t pandeptwidyaop/laravel:php8-alpine3.14 \
     .
 
-docker image push pandeptwidyaop/laravel:php8-alpine3.14-v0.1
+docker image push pandeptwidyaop/laravel:php8-alpine3.14-v0.2
 docker image push pandeptwidyaop/laravel:php8-alpine3.14
